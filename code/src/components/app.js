42ce0c70348708f10e6115690a4a7f46920cb27c
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-04-20 16:44:22 +0200

    Revert "Merge conflicts"
    This reverts commit 4a88c3aeb4c0ad976cebc27534e8834ad6257972.

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -2,26 +2,21 @@ import React from "react"
 import Header from "./header/Header"
 import Form from "./form/form"
 import List from "./List"
-import ListFin from "./listFin"
+import ListFin from "./ListFin"
 import "./app.css"
 import ChooseTasks from "./choosetasks"
 
 class App extends React.Component {
+	state = {
+	  inputValue: "",
+	  todos: [
+	    { value: "Clean kitchen", done: false, date: [] },
+	    { value: "tidy toilet", done: true }
+	  ],
+	  finishedTasks: [{ value: "sleep", done: true }]
+	}
 
-	constructor(props) {
-  super(props)
-	this.state = {
-    inputValue: "",
-    todos: [
-      {value: "Clean the kitchen", done: false},
-      {value: "Tidy the toilet", done: true},
-      {value: "Plant a tree", done: false},
-      {value: "Die", done: false},
-      {value: "Bike to work", done: true}
-    ]
-  }
-}
-
+<<<<<<< HEAD
 	handleSubmit = event => {
 	  event.preventDefault()
 	  const newTodo = {
@@ -35,6 +30,18 @@ class App extends React.Component {
 	handleChooseTask = test => {
 	  console.log(test)
 	}
+=======
+  state = {
+    inputValue: "",
+    todos: [
+      { value: "Clean the kitchen", done: false },
+      { value: "Tidy the toilet", done: true },
+      { value: "Plant a tree", done: false},
+      { value: "Die", done: false},
+      { value: "Bike to work", done: true}
+    ]
+  }
+>>>>>>> d5584fbcb9b09303f34715677a8937894c726b8e
 
 	handleChange = event => {
 	  this.setState({ inputValue: event.target.value })
@@ -46,7 +53,7 @@ class App extends React.Component {
 	  todos[index].done = !todos[index].done
 	  this.setState({ todos })
 
-
+<<<<<<< HEAD
 	  // Skapar nytt objekt och pushar in i ny array
 	  const newFinishedTask = {
 	    value: todos[index].value,
@@ -63,15 +70,21 @@ class App extends React.Component {
 	      <div>
 	        <p>- All calls are in Component App -</p>
 	      </div>
-
 	      <Header />
+=======
+  handleClick = (index) => {
+    const todos = this.state.todos
+    todos[index].done = !todos[index].done
+    this.setState({ todos })
+  }
+>>>>>>> d5584fbcb9b09303f34715677a8937894c726b8e
 
 	      <div>
 	        <p>- Calls for Component Form -</p>
 	        <Form
-						inputValue={this.state.inputValue}
-						handleChange={this.handleChange}
-						handleSubmit={this.handleSubmit} />
+  inputValue={this.state.inputValue}
+  handleChange={this.handleChange}
+  handleSubmit={this.handleSubmit} />
 	      </div>
 
 	      <div>
@@ -79,17 +92,18 @@ class App extends React.Component {
 	        <List todos={this.state.todos} handleClick={this.handleClick} />
 	      </div>
 
+<<<<<<< HEAD
 	      <div>
 	        <p>- Calls for Component FinishedTasks -</p>
 	        <ListFin finishedTasks={this.state.finishedTasks} />
 	      </div>
-
+=======
         <div>
           <p>- Calls for Component Form -</p>
           <Form
-						inputValue={this.state.inputValue}
-						handleChange={this.handleChange}
-						handleSubmit={this.handleSubmit} />
+          inputValue={this.state.inputValue}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit} />
         </div>
 
         <div>
@@ -103,12 +117,16 @@ class App extends React.Component {
           <p>- Calls for Component FinishedTasks -</p>
           <ListFin todos={this.state.todos} />
         </div>
+      </div>
+    )
+  }
+>>>>>>> d5584fbcb9b09303f34715677a8937894c726b8e
 
 	      <div>
 	        <p>-Test Tasks</p>
 	        <ChooseTasks handleChooseTask={this.handleChooseTask} />
 	      </div>
-			</div>
+	    </div>
 	  )
 	}
 }
