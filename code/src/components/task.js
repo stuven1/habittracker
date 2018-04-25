b5934b3b05c80c9bbf80497a19f9af6046b572a0
gunvora <gunvor_a@telia.com>
2018-04-25 13:50:20 +0200

    Styling task

--- code/src/components/task.js
+++ code/src/components/task.js
@@ -6,18 +6,20 @@ class Task extends React.Component {
   render() {
     return (
       <div className="task-container">
-        <div classNmae="task-text-button">
+        <div className="task-text-button">
           {/* // ternary */}
-          <div>className="task-text">
+          <div className="task-text">
             <span style={{ textDecoration: this.props.todo.done ? "line-through" : "none" }}>
               {this.props.todo.value}
             </span>
           </div>
+
           <div className="task-button">
             <button onClick={() => this.props.handleClick(this.props.index)}>
               {this.props.todo.done ? "undo" : "complete"}
             </button>
           </div>
+          
         </div>
       </div>
     )
